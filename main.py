bfdefef5061d0af3e53fda3185efb7b55438ebb8
DreamDoct <160420838+DreamDoct@users.noreply.github.com>
2024-03-21 04:00:11 +0100

    TRASH.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from pydicom import dcmread
 import pydicom
 import glob
 import pathlib
+import shutil
 
 path = (r'C:\Users\danyu\OneDrive\Рабочий стол\dicom tester')
 output_path = (r'C:\Users\danyu\OneDrive\Рабочий стол\dicom destination tester')
@@ -18,6 +19,7 @@ def dicom_scan(InputPath, OutputPath=None, Compression=None):
     start = time.time()
     filesize = 0
     counter = 0
+    directories_df = pd.DataFrame({'PatientName': [], 'StudyID' : [], 'Path' : []})
     patients_df = pd.DataFrame(
         {'PatientName': [], 'DateOfBirth': [], 'StudyDate': [], 'StudyDescription': [], 'Modality': []})
     for dcm_file in pathlib.Path(InputPath).rglob('*'):
@@ -27,18 +29,50 @@ def dicom_scan(InputPath, OutputPath=None, Compression=None):
             filesize = filesize + os.stat(dcm_file).st_size
             patientsname = str(dcm.PatientName)
             patientsname.replace(', ', '')
+            directorypath = str(os.path.abspath(dcm_file))
+            new_row_directories = pd.DataFrame({'PatientName': [patientsname],'StudyID' : [dcm.StudyID] , 'Path' : [directorypath]})
             new_row = pd.DataFrame({'PatientName': [patientsname], 'DateOfBirth': [dcm.PatientBirthDate], 'StudyDate': [dcm.StudyDate], 'StudyDescription': [dcm.StudyDescription], 'Modality': [dcm.Modality]})
             frames = [patients_df, new_row]
+            directory_frames = [directories_df, new_row_directories]
+            directories_df = pd.concat(directory_frames, ignore_index=True)
             patients_df = pd.concat(frames, ignore_index=True)
         except:
             continue
     patients_df = patients_df.drop_duplicates()
     patients_df.reset_index(drop=True,inplace=True)
+    directories_df = directories_df.drop_duplicates()
+    directories_df.reset_index(inplace=True)
+    print(directories_df.head())
+    dirlist = []
+    if OutputPath == None:
+        pass
+    else:
+        counter_output = 0
+        for dcm_file in pathlib.Path(InputPath).rglob('*'):
+            try:
+                dcm = dcmread(str(dcm_file))
+                counter_output = counter_output + 1
+                dcmpath = str(os.path.dirname(os.path.abspath(dcm_file)))
+                dirlist.append(dcmpath)
+            except:
+                continue
+
+
+    directories = []
+    for i in dirlist:
+        if i not in directories:
+            directories.append(i)
+    for path in directories_df['Path']:
+        shutil.move(path, OutputPath)
+
+
+
     print(patients_df)
     print("Number of records: " +str(counter))
     end = time.time()
     elapsed_time = end - start
     print("Elapsed time: " +str(elapsed_time))
+    print(counter_output)
     filesize_mb = filesize/(1024*1024)
     if filesize_mb>1024:
         filesize = filesize_mb/1024
@@ -47,4 +81,4 @@ def dicom_scan(InputPath, OutputPath=None, Compression=None):
         filesize = filesize_mb
         print('File size in MB: ' + str(filesize))
 
-dicom_scan(path)
+dicom_scan(path,output_path)
